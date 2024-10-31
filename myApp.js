42a21cc8c763ca495baaf47e454c054248f7495e
Josh Grant <joshemlego@gmail.com>
2024-10-31 21:06:18 +0000

    Add initial application

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3030;
 
+app.listen(PORT, () => {
+  console.log(`🌹 Useful Programmer Information Security Application Started on Port ${PORT}`);
+});
 
 
 
